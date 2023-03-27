8659024f08628b992c3434d25c76f1130b3455ec
Tim Nguyen <timngyn@gmail.com>
2023-03-26 22:39:47 -0700

    Add js script file

---
+++ .github/workflows/scripts/verify_approvers.js
@@ -0,0 +1,44 @@
+module.exports = {
+  commentListOfReviewers: () => {},
+  verifyApproval: async () => {
+    console.log(context);
+
+    const {
+      payload: {
+        pull_request: { number },
+        repository: {
+          owner: { login: ownerLogin },
+          name
+        },
+        review: {
+          state,
+          user: { login }
+        }
+      }
+    } = context;
+
+    console.log(state);
+    console.log(login);
+    console.log(number);
+    console.log(ownerLogin);
+
+    const reviewers = await github.rest.pulls.listRequestedReviewers({
+      owner: ownerLogin,
+      repo: name,
+      pull_number: number
+    });
+
+    const {
+      data: { users, teams }
+    } = reviewers;
+    // need to also check that all the reviewers actually approved
+
+    const reviews = await github.rest.pulls.listReviews({
+      owner: ownerLogin,
+      repo: name,
+      pull_number: number
+    });
+
+    console.log(reviews.data);
+  }
+};
