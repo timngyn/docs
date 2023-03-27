d21eaf3b417bcc426308c57a99b8d038e6c49ad0
Tim Nguyen <timngyn@gmail.com>
2023-03-26 22:40:29 -0700

    Add js script file

--- .github/workflows/scripts/verify_approvers.js
+++ .github/workflows/scripts/verify_approvers.js
@@ -1,6 +1,6 @@
 module.exports = {
   commentListOfReviewers: () => {},
-  verifyApproval: async () => {
+  verifyApproval: async ({ github, context }) => {
     console.log(context);
 
     const {
