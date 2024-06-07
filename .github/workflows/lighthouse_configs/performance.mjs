18522d7eebe0d41fc9504a0fdf85e1c3ad5d84b2
timngyn <timngyn@gmail.com>
2024-06-07 14:10:29 -0700

    use lighthouse npm package

--- .github/workflows/lighthouse_configs/performance.mjs
+++ .github/workflows/lighthouse_configs/performance.mjs
@@ -1,9 +1,17 @@
-export default {
+/**
+ * @typedef {import('lighthouse').Config} LH.Config
+ */
+
+/** @type {LH.Config} */
+const config = {
   extends: 'lighthouse:default',
   settings: {
-    onlyAudits: [
-      'cumulative-layout-shift',
-      'largest-contentful-paint'
-    ],
+    formFactor: 'desktop',
+    screenEmulation: {
+      mobile: false
+    },
+    onlyCategories: ['performance']
   }
 };
+
+export default config;
